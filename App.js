5629b9153c9d5570765a2a24ceb7bb2eea33a864
lesyhung0501 <91235849+lesyhung0501@users.noreply.github.com>
2022-04-08 17:57:21 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  let [count, setCount] = useState(0);
+
+  function handleIncrease() {
+    setCount(count + 1)
+  }
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={{fontSize:'30px', fontWeight:'bold', color:'red'}}>{count}</Text>
+        <Button
+          title="Increse"
+          onPress={handleIncrease}
+        ></Button>
+        <StatusBar style="auto" />
     </View>
   );
 }
